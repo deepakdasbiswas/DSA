d00d4181015a53d06b8cee147d736ac7037ccb05
Deepak <deepakdasbiswas@gmail.com>
2026-08-26 19:32:06 +0530

    Adding a newnode in the begining(Doubly_linked_list)

---
+++ Linked_Lists/doubly_linked_list.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node{
+	int data;
+	struct Node *prev;
+	struct Node *next;
+}Node;
+Node* addnodefirst(Node *head,int val){
+	Node *newnode=(Node*)malloc(sizeof(Node));
+	newnode->data=val;
+	newnode->prev=NULL;
+	newnode->next=NULL;
+	if(head==NULL){
+		return newnode;
+	}
+	newnode->next=head;
+	head->prev=newnode;
+	return newnode;
+}
