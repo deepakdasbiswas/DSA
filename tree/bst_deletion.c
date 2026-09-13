24fcf9e11639abcd77a67dc94d1f2b7030d9ce70
Deepak <deepakdasbiswas@gmail.com>
2026-09-13 13:46:22 +0530

    tree->Tree

--- tree/bst_deletion.c
+++ tree/bst_deletion.c
@@ -6,16 +6,31 @@ typedef struct TreeNode{
     struct TreeNode *right;
 }TreeNode;
 TreeNode* search(TreeNode *root,int val){
-    if(root==NULL){
-        return root;
-    }
-    
-}
-TreeNode* delete(TreeNode *root,int val){
-    if(root==NULL){
+    if(root==NULL || root->data==val){
         return root;
     }
     if(val<root->data){
-
+        return search(root->left,val);
+    }else if(val>root->data){
+        return search(root->right,val);
     }
+
+}
+TreeNode* delete(TreeNode *root,int val){
+    if(search(root,val)->left==NULL && search(root,val)->right==NULL){
+        free(search(root,val));
+    }else if(search(root,val)->left!=NULL || search(root,val)->right!=NULL){
+        if(search(root,val)->left!=NULL && search(root,val)->right==NULL){
+            TreeNode *temp=search(root,val)->left;
+            search(root,val)->left=search(root,val)->left->left;
+            free(search(root,val)->left);
+        }else if(search(root,val)->left==NULL && search(root,val)->right!=NULL){
+            TreeNode *temp=search(root,val)->right;
+            search(root,val)->right=search(root,val)->right->right;
+            free(search(root,val)->right);
+        }
+    }else if(search(root,val)->left!=NULL && search(root,val)->right!=NULL){
+        
+    }
+    
 }
